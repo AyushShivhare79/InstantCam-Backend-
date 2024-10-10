6c1e654e6c00a3c27eec7eb3ae476210d4d3e76f
AyushShivhare79 <ayushshivhare1008@gmail.com>
2024-10-10 12:43:49 +0530

    feat: created room logic

--- src/index.ts
+++ src/index.ts
@@ -8,55 +8,122 @@ interface userType {
 }
 
 interface RoomType {
-  user1: userType;
-  user2: userType;
+  user1: {};
+  user2: {};
 }
 
-let room: Map<string, RoomType>;
+let room = new Map<string, RoomType>();
 
-// let users: userType[]
+let users = [{}];
 
-let user: WebSocket | null;
-
+// let user: WebSocket | null;
+let userId = 1;
+let count = 0;
+let roomId = 0;
 wss.on("connection", function connection(ws) {
-  // user.push({ socket: ws });
-  // user.
-  if (!user) {
-    user = ws;
-    user.send(JSON.stringify({ message: "Waiting for someone to connect!" }));
-    return;
+  // if (!user) {
+  //   user = ws;
+  //   user.send(JSON.stringify({ message: "Waiting for someone to connect!" }));
+  //   return;
+  // }
+
+  //Creating users
+
+  if (count < 2) {
+    count++;
+    users.push({ id: "user" + userId++, socket: ws });
+    console.log(users);
+    if (count < 2) return;
   }
 
-  // users.push({})
+  //Creating rooms
 
-  ws.send(JSON.stringify({ message: "Paired successful!" }));
+  roomId++;
+  room.set(roomId.toString(), { user1: users[1], user2: users[2] });
+  console.log("ROOM: ", room);
 
-  user.send(JSON.stringify({ user: "user1" }));
-  ws.send(JSON.stringify({ user: "user2" }));
+  // Sending message of pair successful both side
 
-  user.on("message", function message(data: any) {
+  const pair = room.get("1");
+  pair?.user1.socket.send(JSON.stringify({ message: "Paired successful!" }));
+  pair?.user2.socket.send(JSON.stringify({ message: "Paired successful!" }));
+
+  // Giving both thier identity
+
+  pair?.user1.socket.send(JSON.stringify({ user: "user1" }));
+  pair?.user2.socket.send(JSON.stringify({ user: "user2" }));
+
+  //Sharing data with each other
+  pair?.user1.socket.on("message", function message(data: any) {
     const message = JSON.parse(data);
     if (message.type === "createOffer") {
-      ws?.send(JSON.stringify({ type: "createOffer", sdp: message.sdp }));
+      pair?.user2.socket?.send(
+        JSON.stringify({ type: "createOffer", sdp: message.sdp })
+      );
     }
     if (message.type === "iceCandidate") {
-      ws?.send(
+      pair?.user2.socket?.send(
         JSON.stringify({ type: "iceCandidate", candidate: message.candidate })
       );
     }
   });
 
-  ws.on("message", function message(data: any) {
+  pair?.user2.socket.on("message", function message(data: any) {
     const message = JSON.parse(data);
     if (message.type === "createAnswer") {
-      user?.send(JSON.stringify({ type: "createAnswer", sdp: message.sdp }));
+      pair?.user1.socket?.send(
+        JSON.stringify({ type: "createAnswer", sdp: message.sdp })
+      );
     }
     if (message.type === "iceCandidate") {
-      user?.send(
+      pair?.user1.socket?.send(
         JSON.stringify({ type: "iceCandidate", candidate: message.candidate })
       );
     }
   });
+
+  // console.log("DONE: ", users[1]);
+
+  // users.push({})
+  //HERE
+  // ws.send(JSON.stringify({ message: "Paired successful!" }));
+
+  // user.send(JSON.stringify({ user: "user1" }));
+  // ws.send(JSON.stringify({ user: "user2" }));
+
+  // user.on("message", function message(data: any) {
+  //   const message = JSON.parse(data);
+  //   if (message.type === "createOffer") {
+  //     ws?.send(JSON.stringify({ type: "createOffer", sdp: message.sdp }));
+  //   }
+  //   if (message.type === "iceCandidate") {
+  //     ws?.send(
+  //       JSON.stringify({ type: "iceCandidate", candidate: message.candidate })
+  //     );
+  //   }
+  // });
+
+  // ws.on("message", function message(data: any) {
+  //   const message = JSON.parse(data);
+  //   if (message.type === "createAnswer") {
+  //     user?.send(JSON.stringify({ type: "createAnswer", sdp: message.sdp }));
+  //   }
+  //   if (message.type === "iceCandidate") {
+  //     user?.send(
+  //       JSON.stringify({ type: "iceCandidate", candidate: message.candidate })
+  //     );
+  //   }
+  // });
+  //TIll
+  // ws.on("close", () => {
+  //   user?.send(JSON.stringify({ message: "WS user disconnected" }));
+  //   user = null;
+  // });
+
+  // user.on("close", () => {
+  //   ws?.send(JSON.stringify({ message: "User disconnected" }));
+  //   user = null;
+  // });
 
   // user.send(JSON.stringify({ type: 'createOffer' }));
   // user.send("Paired successful!");
