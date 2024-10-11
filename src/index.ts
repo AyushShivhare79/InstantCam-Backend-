0d7cbb79ead9de7e2246168b941ce7e21377e76d
AyushShivhare79 <ayushshivhare1008@gmail.com>
2024-10-11 16:11:23 +0530

    chore: refactor

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,13 @@ interface userType {
 }
 
 interface RoomType {
-  user1: {};
-  user2: {};
+  user1: userType;
+  user2: userType;
 }
 
 let room = new Map<string, RoomType>();
 
 let users = [{}];
-
 // let user: WebSocket | null;
 let userId = 1;
 let count = 0;
@@ -31,20 +30,27 @@ wss.on("connection", function connection(ws) {
 
   if (count < 2) {
     count++;
+    console.log("users: ", users);
     users.push({ id: "user" + userId++, socket: ws });
     console.log(users);
-    if (count < 2) return;
+    if (count >= 2) {
+      //Creating rooms
+
+      roomId++;
+      room.set(roomId.toString(), {
+        user1: users[userId - 2],
+        user2: users[userId - 1],
+      });
+      console.log("ROOM: ", room);
+    } else if (count < 2) {
+      ws.send(JSON.stringify({ message: "Waiting for someone to connect!" }));
+      return;
+    }
   }
-
-  //Creating rooms
-
-  roomId++;
-  room.set(roomId.toString(), { user1: users[1], user2: users[2] });
-  console.log("ROOM: ", room);
 
   // Sending message of pair successful both side
 
-  const pair = room.get("1");
+  const pair = room.get(roomId.toString());
   pair?.user1.socket.send(JSON.stringify({ message: "Paired successful!" }));
   pair?.user2.socket.send(JSON.stringify({ message: "Paired successful!" }));
 
@@ -82,6 +88,21 @@ wss.on("connection", function connection(ws) {
     }
   });
 
+  console.log("userTest0: ", users);
+  users.pop();
+  console.log("userTest1: ", users);
+  users.pop();
+  console.log("userTest2: ", users);
+  count = 0;
+
+  console.log("roomTest0: ", room);
+  console.log("roomId: ", roomId);
+  room.delete(roomId.toString());
+  console.log("roomTest1: ", room);
+  roomId = 0;
+  console.log("roomId: ", roomId);
+
+  userId = 1;
   // console.log("DONE: ", users[1]);
 
   // users.push({})
